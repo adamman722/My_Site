1a855afef6210c9bfb024ce4916c8a5653f8accb
adamman722 <tarpeyadam@gmail.com>
2022-02-01 20:34:21 -0500

    I did thingsssssss

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Input from "../components/Input";
+import SnackbarBlock from "../components/Snackbar/Snackbar";
 function Login(props) {
   return (
     <div className="container">
       <Input bool={props.bool} />
+      <SnackbarBlock />
     </div>
   );
 }
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -3,6 +3,7 @@ import DashboardNavBar from "../components/NavBar";
 import ToDoList from "../components/ToDoList";
 import BuildGrid from "../components/GirdThings";
 import Pokemon from "../components/Pokemon";
+import SnackBar from "../components/Snackbar/Snackbar";
 function ProjectPage() {
   const myName = "Adam";
   return (
@@ -11,6 +12,7 @@ function ProjectPage() {
         <div id="StartOfGirds">
           <BuildGrid addedContent={<ToDoList />} />
           <BuildGrid addedContent={<Pokemon />} />
+          <BuildGrid addedContent={<SnackBar />} />
           <BuildGrid addedContent={"More stuff coming soooooonnnn"} />
         </div>
       )}
